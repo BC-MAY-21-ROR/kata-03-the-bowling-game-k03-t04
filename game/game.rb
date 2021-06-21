a8d2e5d1e3aa29895a365a77826eed2194d25b02
Beritzany <beritzanygc63@gmail.com>
2021-06-21 11:41:04 -0500

    code completion and rspec aggregation rspec implementation and final game structure co-authored-by: Marcos Gil <marcoswaldo52@gmail.com>

--- game/game.rb
+++ game/game.rb
@@ -2,9 +2,31 @@ class Game
     attr_reader :rolls
     attr_reader :score
     def initialize
-        @rolls=1
+        @rolls[]
         @score=0
     
     end
 
+    def pines(pines_knocks)
+        @rolls.push pines_knocks
+    end 
+
+
+    def start_rolls
+        score_board=Score.new(@rolls)
+        cont_roll=0
+        20.times             
+            if score_board.strike?
+                @score += score_board.strikeScore(cont_roll)
+                cont_roll += 1
+            elsif score_board.spare?
+                @score += score_board.spareScore(cont_roll)
+                cont_roll += 2
+            else
+                @score += score_board.frameScore(cont_roll)
+                cont_roll += 2
+            end
+        end    
+        @score
+    end
 end
--- game/score.rb
+++ game/score.rb
@@ -1,6 +1,26 @@
 class Score 
-  def initialize(pines,no_roll)  
-    @pines=pines
-    @no_roll=no_roll
+  def initialize(rolls)  
+    @rolls=rolls
   end
+
+  def spare? cont_roll
+    @rolls[cont_roll] + @rolls[cont_roll + 1] == 10
+  end
+
+  def strike? cont_roll
+    @rolls[cont_roll] == 10
+  end
+
+  def strikeScore cont_roll
+    10 + @rolls[cont_roll + 1] + @rolls[cont_roll + 2]
+  end
+
+  def spareScore cont_roll
+    10 + @rolls[cont_roll + 2]
+  end
+
+  def frameScores cont_roll
+    @rolls[cont_roll] + @rolls[cont_roll + 1]
+  end
+
 end
